550e32b270d0e5a94c5135d2c741644e1a008002
Milan Lustig <mguinangreen726@gmail.com>
2023-02-27 23:41:13 -0500

    Create AutoLimelightSingleAxis.kt

---
+++ src/main/kotlin/frc/robot/commands/AutoLimelightSingleAxis.kt
@@ -0,0 +1,16 @@
+package frc.robot.commands
+
+import cshcyberhawks.swolib.hardware.interfaces.GenericGyro
+import cshcyberhawks.swolib.math.MiscCalculations
+import cshcyberhawks.swolib.math.Vector2
+import cshcyberhawks.swolib.swerve.SwerveDriveTrain
+import edu.wpi.first.math.MathUtil
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
+import edu.wpi.first.wpilibj2.command.CommandBase
+import frc.robot.constants.MiscConstants
+import frc.robot.util.IO
+
+
+class AutoLimelightSingleAxis : CommandBase() {
+    
+}
