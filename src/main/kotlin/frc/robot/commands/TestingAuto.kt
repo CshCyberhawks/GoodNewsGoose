45b9564886385bf26fe846a5309165ea6260f9ce
Ryan <pyan4444@gmail.com>
2023-03-19 16:43:29 -0400

    Update TestingAuto.kt

--- src/main/kotlin/frc/robot/commands/TestingAuto.kt
+++ src/main/kotlin/frc/robot/commands/TestingAuto.kt
@@ -9,25 +9,26 @@ import cshcyberhawks.swolib.limelight.Limelight
 import cshcyberhawks.swolib.math.FieldPosition
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 import TimedFinish
+import frc.robot.subsystems.ArmSystem
 
 // define an empty SequentialCommandGroup
-class TestingAuto(val swerveAuto: SwerveAuto, val gyro: GenericGyro, val limelight: Limelight) :
-        SequentialCommandGroup() {
+class TestingAuto(val swerveAuto: SwerveAuto, val gyro: GenericGyro, val armSystem: ArmSystem) :
+    SequentialCommandGroup() {
 
     // define the constructor
     init {
         gyro.setYawOffset()
         // add the commands to the SequentialCommandGroup
         addCommands(
-                GoToPosition(swerveAuto, FieldPosition(0.0, 2.0, 0.0)),
-                GoToPositionAndExecute(swerveAuto, FieldPosition(0.0, 0.0, 0.0), TimedFinish(5.0)),
-                GoToPosition(swerveAuto, FieldPosition(0.0, 2.0, 0.0)),
+//            GoToPosition(swerveAuto, FieldPosition(0.0, 2.0, 0.0)),
+//            GoToPositionAndExecute(swerveAuto, FieldPosition(0.0, 0.0, 0.0), TimedFinish(5.0)),
+//            GoToPosition(swerveAuto, FieldPosition(0.0, 2.0, 0.0)),
 //                LimeLightAuto(swerveAuto, limelight, .3)
-                // GoToPosition(swerveAuto, FieldPosition(0.0, 0.0, 0.0)),
-                // GoToPosition(swerveAuto, FieldPosition(0.0, 0.0, 0.0)),
-                // GoToPosition(swerveAuto, FieldPosition(-1.0, 2.0, 90.0)),
-                // GoToPosition(swerveAuto, FieldPosition(5.0, 0.0, 90.0))
+            // GoToPosition(swerveAuto, FieldPosition(0.0, 0.0, 0.0)),
+            // GoToPosition(swerveAuto, FieldPosition(0.0, 0.0, 0.0)),
+            // GoToPosition(swerveAuto, FieldPosition(-1.0, 2.0, 90.0)),
+            // GoToPosition(swerveAuto, FieldPosition(5.0, 0.0, 90.0))
 
-                )
+        )
     }
 }
