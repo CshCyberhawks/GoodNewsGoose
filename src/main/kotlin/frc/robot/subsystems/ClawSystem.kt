82a447a23d71643c47c7651e7403fd5ac5e7547c
Ryan <62762243+PY44N@users.noreply.github.com>
2023-04-17 07:47:55 -0400

    Claw Timings

--- src/main/kotlin/frc/robot/subsystems/ClawSystem.kt
+++ src/main/kotlin/frc/robot/subsystems/ClawSystem.kt
@@ -3,6 +3,7 @@ package frc.robot.subsystems
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import com.revrobotics.CANSparkMax
 import com.revrobotics.CANSparkMaxLowLevel
+import cshcyberhawks.swolib.math.MiscCalculations
 import edu.wpi.first.wpilibj.DigitalInput
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import frc.robot.constants.MotorConstants
@@ -17,9 +18,19 @@ class ClawSystem : SubsystemBase() {
     private val clawMotor = CANSparkMax(MotorConstants.clawMotor, CANSparkMaxLowLevel.MotorType.kBrushless)
     private val intakeBeamBreak = DigitalInput(MotorConstants.intakeBeamBreak)
 
+    private var breakTime = -1.0
+
     var clawState = ClawState.Idle
     fun run() {
-        if (clawState == ClawState.Intaking && !intakeBeamBreak.get()) {
+        if (clawState == ClawState.Intaking && !intakeBeamBreak.get() && breakTime == -1.0) {
+            breakTime = MiscCalculations.getCurrentTime()
+        }
+
+        if (clawState != ClawState.Intaking) {
+            breakTime = -1.0
+        }
+
+        if (breakTime != -1.0 && MiscCalculations.getCurrentTime() >= breakTime + 0.1) {
             clawState = ClawState.Idle
         }
 
@@ -27,9 +38,9 @@ class ClawSystem : SubsystemBase() {
         SmartDashboard.putBoolean("Claw Break", intakeBeamBreak.get())
 
         clawMotor.set(when (clawState) {
-            ClawState.Intaking -> -0.8
-            ClawState.Spitting -> 0.5
-            ClawState.Idle -> -0.05
+            ClawState.Intaking -> 0.8
+            ClawState.Spitting -> -0.5
+            ClawState.Idle -> 0.05
         })
 
         SmartDashboard.putNumber("Current", clawMotor.outputCurrent)
